647ad43b65ca02490c0486f5c5bba9691ccaaba5
Prem Goswami <premgoswami2892@gmail.com>
2023-11-21 20:33:58 -0500

    Added all the custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azure: '#6ccff6',
+        lavendar: '#ab81cd',
+        blue: '#2b4570',
+        magnolia: 'f7f0f5',
+        magenta: 'ce84ad'
+      },
+    },
   },
   plugins: [],
 }
